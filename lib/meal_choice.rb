482c47430bf263e830b83a862363e9a5e2970efd
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-01-28 23:54:15 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,8 @@ def meal_choice(protien='tofu',veg1,veg2)
 end
 
 def meal_choice(protien='tofu',veg1,veg2)
-puts "What a nutritious meal!"
-meal = "A plate of #{protien=tofu} with #{veg1} and #{veg2}"
-puts meal
-meal
+  puts "What a nutritious meal!"
+meal = "A plate of #{protien=tofu} with #{veg1} and #{veg2}."
+  puts meal
+  meal
 end
